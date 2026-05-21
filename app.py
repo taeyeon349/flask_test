6de549676cdbc642b97febeb7b62cbaac593b0ba
taeyeon349 <kty010627@gmail.com>
2026-05-21 16:16:32 +0900

    send new project

---
+++ app.py
@@ -0,0 +1,10 @@
+# 플라스크 적용
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':    app.run(debug=True)
